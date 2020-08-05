0f4fb32aa4d34279d8ddf9ad9c63ca3bce4e7e43
Thilakshan Baskaran <telakshanb@gmail.com>
2020-08-05 07:57:21 -0400

    set up for heroku

--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,5 @@
 const mongoose = require('mongoose');
 
-// const UserSchema = mongoose.Schema({
-//     username:{
-//         type: String,
-//         required: true
-//     },email: {
-//         type: String,
-//         required: true
-//     }, password:{
-//         type: String,
-//         required: true
-//     }
-// })
-
-
-
-
-
 const User = mongoose.model('User',{
     username: {
         type: String,
